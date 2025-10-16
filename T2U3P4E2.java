42c54fe265c8442326538fca2d997892df67322a
JocelynSantiagoP <santipuenjocelyn546@gmail.com>
2025-10-15 19:22:55 -0700

    Add files via upload

---
+++ T2U3P4E2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t2u3p4e2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class T2U3P4E2 {
+
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <=100){
+            System.out.println(+i);
+            i = i+5;
+        }
+    }
+}
